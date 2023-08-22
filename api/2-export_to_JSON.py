e7bf37b77c6a0e7a42194d497a81a0942230d147
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-21 20:01:16 -0700

    feat: add Python script to export data in the JSON format

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script that, using a REST API and export data in the CSV format"""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    u = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    t = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+    user = requests.get(u)
+    todo = requests.get(t)
+    data = []
+    for i in todo.json():
+        new = {'task': f"{i.get('title')}",
+               'completed': f"{i.get('completed')}",
+               'username': f"{user.json().get('username')}"}
+        data.append(new)
+    dicc = {f"{argv[1]}": data}
+    with open(f"{argv[1]}.json", 'w') as f:
+        json.dump(dicc, f)
